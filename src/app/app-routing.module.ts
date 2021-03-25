9a81fec30bef682feae7347d760590b93468a090
quares-mpalmieri <mpalmieri@quaresitsolutions.com>
2021-03-25 18:42:16 -0300

    Se trabaja en el navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,16 +8,14 @@ const routes: Routes = [
 
   { path: '', redirectTo:'/home' , pathMatch:'full'},
   
-  { path: 'quienSoyComponent-component', component: QuienSoyComponent },
-
+  { path: 'quienSoy', component: QuienSoyComponent },
  
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-
  
   { path: 'login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) },
-
  
   { path: 'register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) },
+
 ];
 
 @NgModule({
